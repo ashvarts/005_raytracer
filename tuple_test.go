b6beda615d56a48ff0bfec83b9265c0ec279bd18
Arthur Shvarts <ashvarts@gmail.com>
2022-07-21 16:30:48 -0400

    refactored into table-driven tests

--- tuple_test.go
+++ tuple_test.go
@@ -1,14 +1,65 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestTupleIsPoint(t *testing.T) {
-	tup := tuple{4.3, -4.2, 3.1, 1.0}
-	if tup.isPoint() != true {
-		t.Error("tuple is point should be true")
+func Test_tuple_isPoint(t *testing.T) {
+	type fields struct {
+		x float64
+		y float64
+		z float64
+		w float64
 	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{"isPoint", fields{4.3, -4.2, 3.1, 1.0}, true},
+		{"isPoint", fields{4.3, -4.2, 3.1, 0.0}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tr := tuple{
+				x: tt.fields.x,
+				y: tt.fields.y,
+				z: tt.fields.z,
+				w: tt.fields.w,
+			}
+			if got := tr.isPoint(); got != tt.want {
+				t.Errorf("tuple.isPoint() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
-	if tup.isVector() != false {
-		t.Error("tuple is vector should be false")
+func Test_tuple_isVector(t *testing.T) {
+	type fields struct {
+		x float64
+		y float64
+		z float64
+		w float64
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{"isVector", fields{4.3, -4.2, 3.1, 1.0}, false},
+		{"isVector", fields{4.3, -4.2, 3.1, 0.0}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tr := tuple{
+				x: tt.fields.x,
+				y: tt.fields.y,
+				z: tt.fields.z,
+				w: tt.fields.w,
+			}
+			if got := tr.isVector(); got != tt.want {
+				t.Errorf("tuple.isVector() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
