805be412fb0b702844036724f70e2cb18b625117
Arthur Shvarts <arthur.shvarts@commercetools.com>
2022-10-26 21:40:11 -0400

    redoing matrix as slice of sclices

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -1,11 +1,3 @@
 package matrix
 
-type Dim struct {
-	R, C int
-}
-
 type Matrix [][]float64
-
-func NewMatrix(r, c int) Matrix {
-	return make(Matrix, r*c)
-}
--- matrix/matrix_test.go
+++ matrix/matrix_test.go
@@ -1,14 +1,24 @@
 package matrix
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewMatrix(t *testing.T) {
+	assert := assert.New(t)
 	m := Matrix{
-		[]float64{24, 23},
-		[]float64{4, 5},
+		[]float64{1, 2, 3, 4},
+		[]float64{5.5, 6.5, 7.5, 8.5},
+		[]float64{9, 10, 11, 12},
+		[]float64{13.5, 14.5, 15.5, 16.5},
 	}
-	fmt.Println(m)
+	assert.Equal(m[0][0], 1.0)
+	assert.Equal(m[0][3], 4.0)
+	assert.Equal(m[1][0], 5.5)
+	assert.Equal(m[1][2], 7.5)
+	assert.Equal(m[2][2], 11.0)
+	assert.Equal(m[3][0], 13.5)
+	assert.Equal(m[3][2], 15.5)
 }
